551cc1f2e7f5cb0a77e2e4a9024fed0bf87d75c5
JErickDev <jerickgm89@gmail.com>
2025-08-21 21:57:51 -0500

    Refactor: Config anual of files and archives for Qtile 2025

--- nvim/lua/plugins/oil.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "stevearc/oil.nvim",
-  opt = {},
-  -- Optional dependencies
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  -- kjdependencies = { { "echasnovski/mini.icons", opts = {} } },
-}
